d57adc2919f5ae2c07773ca2274f7ac0b117d7ec
S.Mant1s <dss23112010@gmail.com>
2026-02-15 18:03:07 +0300

    Update worker.js

--- worker.js
+++ worker.js
@@ -7,7 +7,7 @@ export default {
 
     const headers = {
       "Content-Type": "text/plain; charset=utf-8",
-      "Subscription-Userinfo": "upload=0;download=0;total=107374182400;expire=1893456000",
+      "Subscription-Userinfo": "upload=0;download=0;total=0;expire=1893456000",
       "Cache-Control": "no-store, no-cache, must-revalidate",
       "Access-Control-Allow-Origin": "*"
     };

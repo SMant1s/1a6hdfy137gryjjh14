389baf742cdd618701a421e1f07fa73a1db1ed89
S.Mant1s <dss23112010@gmail.com>
2026-02-15 17:43:49 +0300

    Update index.js

--- function/index.js
+++ function/index.js
@@ -1,7 +1,6 @@
 export default {
   async fetch(request) {
     const configs = [
-      "vless://736c9bd2-842a-492e-83e4-18d92a7416c7@81.scroogethebest.com:443?type=tcp&security=reality&flow=xtls-rprx-vision&fp=random&pbk=_clqzJ8ZkSG71dSLu5KfRB9NJf63uBbdlDFZ4hp1uBE&sid=3d9bfa9202efd228&sni=m.vk.ru&path=&host=&spx=/#%F0%9F%87%A9%F0%9F%87%AA%20Germany%20%7C%20%F0%9F%8C%90%20Anycast-IP%20%5B%2ACIDR%5D",
       "vless://uuid@ip:port?security=reality...#Server2"
     ].join('\n');
 
